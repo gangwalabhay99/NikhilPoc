b41d2aa61d725eb777f88760d7afecffab5bb2fa
nikhil-insight <137178117+nikhil-insight@users.noreply.github.com>
2023-08-10 18:23:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 import json
-import os
 from jinja2 import Template
+import os
+import builtins
 
 with open("test.json") as jsondata:
     allresults=json.load(jsondata)["profiles"][0]["controls"][0]['results']
     
-#for result in allresults:
- 
- #print(result["status"])
- #print(result["code_desc"])
- 
+list_resource_type = []
+list_time_taken =[]
 
+for result in allresults:
+   
+    list_resource_type.append(result["code_desc"].split('-')[0])
+    list_time_taken.append(result["run_time"])
+
+
+ 
+ #{%- for result in allresults & time in list_time_taken & type in list_resource_type %}
+
+context = {
+ 'list_time_taken':list_time_taken,
+ 'list_resource_type':list_resource_type,
+ 'allresults':allresults
+}
+
+context['zip'] = builtins.zip
 
 with open("template.md", "r") as f:
  template = Template(f.read())
 
- markdown = template.render(allresults=allresults)
+ markdown = template.render(context)
 
- print(markdown)
+
+ 
+
+ #with open("table.md", "w") as f:
+  #f.write(markdown)
 
 with open(os.environ["GITHUB_STEP_SUMMARY"], "w") as f:
   f.write(markdown)
