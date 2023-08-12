1cf4950e6e3d2a66e16dbec224d6b0f5e46d97b8
nikhil-insight <137178117+nikhil-insight@users.noreply.github.com>
2023-08-12 21:06:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 from jinja2 import Template
 import os
 import builtins
+import requests
+
 
 with open("test.json") as jsondata:
     allresults=json.load(jsondata)["profiles"][0]["controls"][0]['results']
@@ -37,6 +39,52 @@ with open("template.md", "r") as f:
  #with open("table.md", "w") as f:
   #f.write(markdown)
 
-with open(os.environ["GITHUB_STEP_SUMMARY"], "w") as f:
-  f.write(markdown)
+#with open(os.environ["GITHUB_STEP_SUMMARY"], "w") as f:
+  #f.write(markdown)
 
+
+
+repository_full_name = os.getenv("GITHUB_REPOSITORY")
+owner = repository_full_name.split("/")[0]
+repo = repository_full_name.split("/")[1]
+
+repository_owner = owner
+repository_name = repo
+
+# Set your GitHub token as an environment variable or replace with your actual token
+github_token = os.environ.get("GITHUB_TOKEN")
+
+# Set the commit SHA for which you want to create the check run
+commit_sha = os.getenv("GITHUB_SHA")
+
+# Set the URL for creating a check run
+check_run_url = f"https://api.github.com/repos/{repository_owner}/{repository_name}/check-runs"
+
+# Set the headers for the API request
+headers = {
+    "Authorization": f"Bearer {github_token}",
+    "Accept": "application/vnd.github.antiope-preview+json"
+}
+
+# Set the payload for creating the check run
+payload = {
+    "name": "Detailed Result",
+    "head_sha": commit_sha,
+    "status": "completed",
+    "conclusion": "success",
+    "output": {
+        "title": "Detailed Results",
+        "summary": markdown
+        
+    }
+}
+
+# Send the API request to create the check run
+response = requests.post(check_run_url, json=payload, headers=headers)
+
+# Check if the API request was successful
+if response.status_code == 201:
+    print("Check run created successfully!")
+else:
+    print(f"Failed to create check run. Status code: {response.status_code}")
+    print("Response:", response.text)
